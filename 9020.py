7d44e03ef3d467960e82865f23147d01ca4e9573
엄상욱 <sanguookmax@gmail.com>
2021-04-21 14:27:10 +0000

    97-shape 골드바흐의 추측 (?) pypy로 제출하니 시간초과가 안남 왜? https://www.acmicpc.net/board/view/48462

--- 9020.py
+++ 9020.py
@@ -1,21 +1,18 @@
 import sys
 
-def erasto(n):
-    Prime_list = [True] * n
-    for i in range(2, int(n ** (1/2))+1):
+def erasto():  # 아리스토텔래스의 채 (미리 완성 > 시간단축)
+    Prime_list = [True] * 10000
+    for i in range(2, 101):
         if i:
-            for j in range(i*i, n, i):
+            for j in range(i*i, 10000, i):
                 Prime_list[j] = False
-    return [i for i in range(2, n) if Prime_list[i] is True]
+    return Prime_list
 
 def check_Goldbach(n):
-    Primes = erasto(n)
-    sub = 0
-    for i in Primes:
-        if Primes.count(n-i) == 1 and sub <= n-(2*i):
-            a = i
-            b = n-i
-    return print(a, b)
+    Primes = erasto()
+    for i in range(n//2, n, 1):  # 중앙 가장 큰 값부터 확인
+        if Primes[n-i] and Primes[i]:
+            return print(n-i, i)
 
 t = int(sys.stdin.readline().rstrip())
 
