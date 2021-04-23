3af8d3994c4e9880ce6f1569d84540fdf4819a10
엄상욱 <sanguookmax@gmail.com>
2021-04-23 13:37:00 +0000

    골드바흐의 추측 https://www.acmicpc.net/problem/9020

--- 9020.py
+++ 9020.py
@@ -8,14 +8,13 @@ def erasto():  # 아리스토텔래스의 채 (미리 완성 > 시간단축)
                 Prime_list[j] = False
     return Prime_list
 
-def check_Goldbach(n):
-    Primes = erasto()
+def check_Goldbach(n, Primes):
     for i in range(n//2, n, 1):  # 중앙 가장 큰 값부터 확인
         if Primes[n-i] and Primes[i]:
             return print(n-i, i)
 
 t = int(sys.stdin.readline().rstrip())
-
+Primes = erasto()
 for i in range(t):
     n = int(sys.stdin.readline().rstrip())
-    check_Goldbach(n)
+    check_Goldbach(n, Primes)
