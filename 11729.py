39e2f98cd68640417187c621adfa22ca61577390
엄상욱 <sanguookmax@gmail.com>
2021-05-09 15:03:26 +0000

    하노이 탑 이동 순서 https://www.acmicpc.net/problem/11729

--- 11729.py
+++ 11729.py
@@ -1,14 +1,16 @@
 import sys
 
 def hanoi(n, start, via, end):
-    if n == 1:
-        print(start, end)
+    if n == 1:  # 'start' 막대에 있는 가장 작은 원판을 'end' 막대로
+        logs.append([start, end])
     else:
         hanoi(n-1, start, end, via)
-        print(start, via)
-        
-
+        logs.append([start, end])
+        hanoi(n-1, via, start, end)
 
 n = int(sys.stdin.readline().rstrip())
+logs = []  # list 안쓰는 방법이 있을까?
 
-
+hanoi(n, 1, 2, 3)
+print(len(logs))
+print('\n'.join([' '.join(str(i) for i in log) for log in logs]))
