b0eaf14e1381a80d2d3584e585040b46e77aa5f9
엄상욱 <sanguookmax@gmail.com>
2021-07-27 15:01:15 +0000

    7/28

--- baekjoon_algo/basic/17298.py
+++ baekjoon_algo/basic/17298.py
@@ -2,16 +2,15 @@ import sys
 
 n = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
-l = len(nums)
-result = []
 
-for i in range(n):
-    for j in range(i, l):
-        if nums[i] < nums[j]:
-            result.append(nums[j])
-            break
-    if len(result) < i+1:
-        result.append(-1)
+result = [-1] * n
+stack = [0]
 
-for i in result:
-    print(i, end=' ')
+for i in range(1, n):
+    # stack에 nums[i]보다 작은 모든 값을 pop함
+    while stack and nums[stack[-1]] < nums[i]:
+        result[stack.pop()] = nums[i]
+    stack.append(i)
+
+for j in result:
+    print(j, end=' ')
